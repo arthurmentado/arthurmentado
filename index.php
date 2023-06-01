e8f10ce00d69c5d1c00cf0b9c056a28dd308754b
Arthur <arthursantanatec@gmail.com>
2023-05-31 23:57:19 -0300

    adicionando o design

--- index.php
+++ index.php
@@ -15,15 +15,54 @@
             <div>
                 <h3>Me chamo</h3>
                 <h1>Arthur Santana</h1>
-                <h2>Desenvolvedor Web</h2>
-                <button>Download CV</button>
+                <h2>Desenvolvedor Front End | Desenvolvedor Back End</h2>
+                <div class="buttonCVDownload">
+                    <button class="buttonCV">Download CV</button>
+                </div>
             </div>
             <div>
                 <img src="imgs/computador.png" alt="Computador">
             </div>
         </section>
         <section id="sobreMim">
+            <div class="container">
+                <div class="photo">
+                    <img src="imgs/fotoArthur.jpg" alt="Foto de Arthur" id="foto">
+                </div>
+                <div class="sobreTxt">
+                    <h2>Sobre Mim</h2>
+                    <p> Meu nome é Arthur Santana e sou um desenvolvedor de 18 anos, apaixonado por criar soluções digitais e explorar o mundo da programação.
+                        Embora eu ainda não tenha entrado no mercado de trabalho, tenho investido meu tempo em aprender e aprimorar minhas habilidades como desenvolvedor.
+                    </p>
+                    <p>
+                        Tenho interesse em desenvolvimento front end e back end e amo criar soluções para a WEB.
+                    </p>
+                    <p>
+                        Estou animado para embarcar nesta jornada como desenvolvedor e estou pronto para enfrentar novos desafios e aprimorar minhas habilidades constantemente.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section id="projetos">
+            <div>
+                <h2>Projetos</h2>
+            </div>
+        </section>
+        <section id="skills">
+            <h2>Skills</h2>
+            <div class="container">
 
+            </div>
+        </section>
+        <section>
+            <div id="certificacoes">
+                <h2>Certificações</h2>
+                <p>Ensino técnico em Desenvolvimento de Sistemas - ETEC <br>
+                (Início: Fevereiro/2022 - Término: Junho/2023)</p>
+                <p>Ciência da Computação - MACKENZIE <br>
+                (Início: Agosto/2023 - Término previsto: Agosto/2028)</p>
+
+            </div>
         </section>
     </main>
     <footer>
