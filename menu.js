0417ade65ed8d1d481a58df31efa274dd12effdd
Arthur <arthursantanatec@gmail.com>
2023-06-10 14:34:16 -0300

    deixando em html e fazendo página de obrigado

--- menu.js
+++ menu.js
@@ -1,3 +1,14 @@
+const list = document.querySelectorAll('nav a')
+    function activeLink(){
+        list.forEach((item) =>
+        item.classList.remove('active'))
+        this.classList.add('active')
+    }
+        list.forEach((item) =>
+        item.addEventListener('click', activeLink))
+
+    
+
 let menuIcon = document.querySelector(".menuIcon")
 let nav = document.querySelector(".nav")
 let menu = document.querySelector(".menu")
--- script.js
+++ script.js
@@ -57,3 +57,22 @@ const contato = document.querySelector("#contato")
         projetos.classList.toggle("darkMode")
         contato.classList.toggle("darkMode")
     }
+    
+
+    // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
